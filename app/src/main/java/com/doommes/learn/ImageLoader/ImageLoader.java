e74b4bc71336975a6fe52db68d45f621ca76e2c6
ldq <autoFerdinand@163.com>
2019-03-01 18:05:33 +0800

    imageLoader

--- app/src/main/java/com/doommes/learn/ImageLoader/ImageLoader.java
+++ app/src/main/java/com/doommes/learn/ImageLoader/ImageLoader.java
@@ -10,8 +10,13 @@ import android.support.v4.util.LruCache;
 
 import com.jakewharton.disklrucache.DiskLruCache;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class ImageLoader {
     private static final int DISK_CACHE_MAXSIZE = 10 * 1024 * 1024;
@@ -77,4 +82,37 @@ public class ImageLoader {
     private Bitmap loadBitmapFromHttp(String url, int reqWidth, int reqHeight){
         return null;
     }
+
+    public boolean downloadUrlToStream(String urlString, OutputStream outputStream){
+        HttpURLConnection urlConnection = null;
+        BufferedOutputStream out = null;
+        BufferedInputStream in = null;
+
+        try {
+            URL url = new URL(urlString);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            in = new BufferedInputStream(urlConnection.getInputStream(), 1 * 1024);
+            out = new BufferedOutputStream(outputStream, 8*1024);
+            int b;
+            while ((b = in.read()) != -1){
+                out.write(b);
+            }
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (urlConnection != null){
+                urlConnection.disconnect();
+            }
+            try {
+                if (out != null) out.close();
+                if (in != null) in.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+        }
+        return false;
+    }
+
 }
